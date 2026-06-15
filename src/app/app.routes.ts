f3e8533cb8fc704858ad2116c62c2a6f458c8b7d
bagleo_b <bagleo_b@f112.ub.lan>
2026-06-15 11:09:59 +0800

    Added routing & navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { Kanto } from './kanto/kanto';
+import { Johto } from './johto/johto';
+import { Hoenn } from './hoenn/hoenn';
+import { Pokemart } from './pokemart/pokemart';
+
+export const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+
+  {
+    path:'home',
+    component:Home
+  },
+
+  {
+    path:'kanto',
+    component:Kanto
+  },
+
+  {
+    path:'johto',
+    component:Johto
+  },
+
+  {
+    path:'hoenn',
+    component:Hoenn
+  },
+
+  {
+    path:'pokemart',
+    component:Pokemart
+  }
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import {
+RouterOutlet,
+RouterLink
+} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector:'app-root',
+  standalone:true,
+  imports:[
+    RouterOutlet,
+    RouterLink
+  ],
+  templateUrl:'./app.html',
+  styleUrl:'./app.css'
 })
-export class App {
-  protected readonly title = signal('pokemon-regions-app');
-}
+export class App{}
