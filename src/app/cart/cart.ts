0b38689040c501c2e866cc6496824d6d2fba2bb2
bagleo_b <bagleo_b@f112.ub.lan>
2026-06-16 09:09:35 +0800

    Fix Pokemart service imports Error

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PokemartService } from '../pokemart-service';
 
 @Component({
   selector: 'app-cart',
+  standalone: true,
   imports: [],
   templateUrl: './cart.html',
-  styleUrl: './cart.css',
+  styleUrl: './cart.css'
 })
-export class Cart {}
+export class Cart {
+
+  pokemartService = inject(PokemartService);
+
+}
